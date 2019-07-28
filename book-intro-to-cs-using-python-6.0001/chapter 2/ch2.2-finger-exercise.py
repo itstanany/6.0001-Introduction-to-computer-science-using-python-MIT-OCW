1985ab04117b2ef6b1ed8a0c7d5a65149026403b
Ahmad ALy Mohamad ALy Ramadan Tanany <AhmadAlyMohamad@gmail.com>
2019-07-28 20:16:11 +0200

    Adding question to th e file in a comment

--- book-intro-to-cs-using-python-6.0001/chapter 2/ch2.2-finger-exercise.py
+++ book-intro-to-cs-using-python-6.0001/chapter 2/ch2.2-finger-exercise.py
@@ -2,7 +2,12 @@
 """
 Created on Sun Jul 28 16:14:02 2019
 
-@author: Tanany
+@author: Ahmed A. T.
+chapter 2 section 2 --- FINGER EXERCISE ---
+******* 
+Finger exercise: Write a program that examines three variables—x, y, and z—and
+prints the largest odd number among them. If none of them are odd, it should
+print a message to that effect. *********
 """
 
 # print the latgest odd input
