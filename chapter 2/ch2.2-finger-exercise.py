174f6c107bc910aab1c86c7e756af5e8fbbadc5a
Ahmad ALy Mohamad ALy Ramadan Tanany <AhmadAlyMohamad@gmail.com>
2019-07-28 17:22:26 +0200

    get input from the user

--- chapter 2/ch2.2-finger-exercise.py
+++ chapter 2/ch2.2-finger-exercise.py
@@ -5,4 +5,8 @@ Created on Sun Jul 28 16:14:02 2019
 @author: Tanany
 """
 
-fg
+# print the latgest odd input
+print('Enter Three Numbers to Print the Largest Odd Number Entered')
+x = float(int(input('Please enter a Number X ')))
+y = float(int(input('Please enter a Number Y ')))
+z = float(int(input('Please enter a Number Z ')))
