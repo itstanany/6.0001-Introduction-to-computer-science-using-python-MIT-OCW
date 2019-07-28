572d2d4b52f93a85c9990b49db61a15331185897
Ahmad ALy Mohamad ALy Ramadan Tanany <AhmadAlyMohamad@gmail.com>
2019-07-28 17:23:48 +0200

    in case all uset inout are odd

--- chapter 2/ch2.2-finger-exercise.py
+++ chapter 2/ch2.2-finger-exercise.py
@@ -10,3 +10,12 @@ print('Enter Three Numbers to Print the Largest Odd Number Entered')
 x = float(int(input('Please enter a Number X ')))
 y = float(int(input('Please enter a Number Y ')))
 z = float(int(input('Please enter a Number Z ')))
+
+# first check wheter the three are odd
+if x%2 != 0 and y%2 != 0 and z%2 != 0:
+    if x < y and y < z:
+        print("The Largest Odd Number is Z =", z)
+    elif z < x:
+        print("The Largest Odd Number is X =", x)
+    elif z < y:
+        print("The Largest Odd Number is Y =", y)
