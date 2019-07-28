1c0231a3be209e9cbb54219c668d9c2b2068fc2a
Ahmad ALy Mohamad ALy Ramadan Tanany <AhmadAlyMohamad@gmail.com>
2019-07-28 20:26:43 +0200

    Initializing file for the first finger exercise chapter 2 section 4

---
+++ book-intro-to-cs-using-python-6.0001/chapter 2/ch2.4-finger-execise-1.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 28 20:19:12 2019
+
+@author: Ahmed Ali
+--- chapter 4 section 4 ---
+Finger Exercise 1
+
+*******
+Finger exercise: Replace the comment in the following code with a while loop.
+numXs = int(input('How many times should I print the letter X? '))
+toPrint = ''
+#concatenate X to toPrint numXs times
+print(toPrint)
+*******
+"""
+
+
+
