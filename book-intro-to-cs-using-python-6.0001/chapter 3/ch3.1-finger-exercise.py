fa4b7501f42b603cfcb4d20423c05d14476795e7
Ahmad ALy Mohamad ALy Ramadan Tanany <AhmadAlyMohamad@gmail.com>
2019-07-30 14:39:56 +0200

    ceating folder for ch3 and initialization file for section1

---
+++ book-intro-to-cs-using-python-6.0001/chapter 3/ch3.1-finger-exercise.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 30 14:35:41 2019
+
+@author: Ahmed Ali
+---
+chapter 3 section 1
+---
+*******
+Finger exercise: Write a program that asks the user to enter an integer and prints
+two integers, root and pwr, such that 0 < pwr < 6 and root**pwr is equal to the integer entered by the user. If no such pair of integers exists, it should print a message to that effect.
+*******
+"""
+
