2aefe00fc1d475f6a5a5c6870436d19719cc37bb
Ahmad ALy Mohamad ALy Ramadan Tanany <AhmadAlyMohamad@gmail.com>
2019-07-30 14:43:54 +0200

    initialization file for section2

---
+++ book-intro-to-cs-using-python-6.0001/chapter 3/ch3.2-finger-exercise.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 30 14:42:12 2019
+
+@author: Ahmed A T
+--- 
+chapter 3 section 2
+---
+*******
+Finger exercise: Let s be a string that contains a sequence of decimal numbers
+separated by commas, e.g., s = '1.23,2.4,3.123'. Write a program that prints the
+sum of the numbers in s.
+*******
+"""
+
