41a24079f0cf7614e98b0df6978d784327f3eadd
Ahmad ALy Mohamad ALy Ramadan Tanany <AhmadAlyMohamad@gmail.com>
2019-08-09 22:16:07 +0200

    initializing the file of finger exercise 3.4

--- book-intro-to-cs-using-python-6.0001/chapter 3/ch3.4-finger-exercise.py
+++ book-intro-to-cs-using-python-6.0001/chapter 3/ch3.4-finger-exercise.py
@@ -3,6 +3,11 @@
 Created on Fri Aug  9 22:13:49 2019
 
 @author: Ahmed Ali
-
+-------
+chapter 3 section 4
+-------
+*******
+Finger exercise: What is the decimal equivalent of the binary number 10011?
+*******
 """
 
