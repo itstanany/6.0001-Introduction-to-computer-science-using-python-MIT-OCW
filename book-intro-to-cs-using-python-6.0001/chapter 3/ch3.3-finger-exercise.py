1ab32f9ff21f58613b9cfff7a18bff58fefc5bc6
Ahmad ALy Mohamad ALy Ramadan Tanany <AhmadAlyMohamad@gmail.com>
2019-08-09 21:41:06 +0200

    add the solution

--- book-intro-to-cs-using-python-6.0001/chapter 3/ch3.3-finger-exercise.py
+++ book-intro-to-cs-using-python-6.0001/chapter 3/ch3.3-finger-exercise.py
@@ -13,4 +13,27 @@ numbers? (Hint: think about changing low to ensure that the answer lies within
 the region being searched.)
 ********
 """
+# the solution
+real_input = float(input('uuuuuPlease, Enter a perfect square: '))
+u = abs(real_input) # u = absolute input
+low = 0.0
+high = max(1.0, u)
+uNumberOfGuesses = 0
+uepsilon = 0.01
+uans = (high + low)/2
+while abs(uans**3 - u) >= uepsilon:
+    uNumberOfGuesses += 1
+    if uans**3 < u:
+        low = uans
+    else:
+        high = uans
+    uans = (high + low)/2
+print('uuuuuuuNumberOfGuesses:', uNumberOfGuesses)
+if real_input < 0:
+    uans= -uans
+if abs(abs(uans**3) - u) >= uepsilon:
+    print('Can\'t find cube root of:', u)
+else:
+    print(uans, 'is close to square root of:', u)
+    
 
