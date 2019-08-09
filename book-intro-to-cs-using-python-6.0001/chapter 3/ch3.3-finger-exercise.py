bca59aa7b29bd24a729f28b07fc59265355ae8de
Ahmad ALy Mohamad ALy Ramadan Tanany <AhmadAlyMohamad@gmail.com>
2019-08-09 21:40:21 +0200

    ading the exercise o the file

--- book-intro-to-cs-using-python-6.0001/chapter 3/ch3.3-finger-exercise.py
+++ book-intro-to-cs-using-python-6.0001/chapter 3/ch3.3-finger-exercise.py
@@ -3,5 +3,14 @@
 Created on Fri Aug  9 21:36:08 2019
 
 @author: Ahmed Ali
+--------
+chapter 3 section 3
+--------
+********
+Finger exercise: What would have to be changed to make the code in Figure 3.4
+work for finding an approximation to the cube root of both negative and positive
+numbers? (Hint: think about changing low to ensure that the answer lies within
+the region being searched.)
+********
 """
 
