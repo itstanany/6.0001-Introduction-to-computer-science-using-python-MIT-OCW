39a12eb23b7b93aa5aef4558c1e088662090f9e6
Ahmad ALy Mohamad ALy Ramadan Tanany <AhmadAlyMohamad@gmail.com>
2019-08-14 13:40:09 +0200

    Adding solution to PART A Problem Set One

--- 6.0001-problem-sets-solution/problem-set-one-6.0001/ps1a.py
+++ 6.0001-problem-sets-solution/problem-set-one-6.0001/ps1a.py
@@ -47,4 +47,16 @@ program print results in the format shown in the test cases below.
 
 *******
 """
-
+# THE SOLUTION TO PART A OF PROBLEM SET ONE
+annual_salary = float(input('Enter your annual salary: '))
+portion_saved = float(input('Enter the percent of your salary to save, as a decimal: '))
+total_cost = float(input('Enter the cost of your dream home: '))
+portion_down_payment = total_cost * 0.25
+monthly_salary = annual_salary/12
+portion_saved_monthly = monthly_salary * portion_saved
+current_savings = 0
+months = 0
+while current_savings < portion_down_payment:
+    current_savings = current_savings + (current_savings * (0.04/12)) + portion_saved_monthly
+    months = months + 1
+print('Number of Months:', months)
