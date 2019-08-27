ed297397f57bfe961c2c933a60dfcef7010e28b6
Ahmad ALy Mohamad ALy Ramadan Tanany <AhmadAlyMohamad@gmail.com>
2019-08-27 17:35:23 +0200

    Initializing file for finger exercise ch4.1

---
+++ book-intro-to-cs-using-python-6.0001/chapter 4/ch4.1-finger-exercise.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 27 17:32:07 2019
+
+@author: Ahmed Ali
+-------
+Finger exercise Chapter 4 section 1
+-------
+*******
+Finger exercise: Write a function isIn that accepts two strings as arguments and
+returns True if either string occurs anywhere in the other, and False otherwise.
+Hint: you might want to use the built-in str operation in.
+*******
+"""
+
