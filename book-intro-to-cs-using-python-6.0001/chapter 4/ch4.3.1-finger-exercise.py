be846035638211fbebab3341b884d3919bbffa96
Ahmad ALy Mohamad ALy Ramadan Tanany <AhmadAlyMohamad@gmail.com>
2019-08-29 19:44:14 +0200

    adding code to track any number

--- book-intro-to-cs-using-python-6.0001/chapter 4/ch4.3.1-finger-exercise.py
+++ book-intro-to-cs-using-python-6.0001/chapter 4/ch4.3.1-finger-exercise.py
@@ -16,3 +16,34 @@ fib(5), how many times does it compute the value of fib(2) on the way to computi
 '''
 The solution is '3' three times
 '''
+
+# code to track how many times the specific has been called
+number_to_fib = int(input('Please, Enter a positive integer: '))
+# the umber user want to track how many it is called during getting Fibonacci of the input
+specific_number_to_be_counted = int(input('Please, Enter a positive integer to track how many it is called? '))
+def fib(n):
+    '''
+    Assumes, n int >= 0
+    return fibbonachi of n
+    '''
+    global numFibCalls
+    numFibCalls = 0
+    numFibCalls += 1
+    global numFibTwo
+    if n == specific_number_to_be_counted:
+        numFibTwo += 1
+    if n == 1 or n == 0:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+
+def testFib(x):
+    global numFibCalls
+    numFibCalls = 0
+    global numFibTwo
+    numFibTwo = 0
+    print('the Fibbonaci number of:', x, 'is', fib(x))
+    print('Fib function has been called', numFibCalls, 'times')
+    print('Fib(',specific_number_to_be_counted,') function has been called', numFibTwo, 'times')
+    return None
+testFib(number_to_fib)
