994a6702d6af13705c599883621decc0534ef97d
Ahmad ALy Mohamad ALy Ramadan Tanany <AhmadAlyMohamad@gmail.com>
2019-08-29 15:09:57 +0200

    Initializing file for finger exercise 4.3.1

---
+++ book-intro-to-cs-using-python-6.0001/chapter 4/ch4.3.1-finger-exercise.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 29 15:06:21 2019
+
+@author: Ahmed Ali Mohamed
+-------
+Chapter 4 section 3.1   (4.3.1)
+-------
+
+*******
+Finger exercise: When the implementation of fib in Figure 4.7 is used to compute
+fib(5), how many times does it compute the value of fib(2) on the way to computing fib(5)?
+*******
+"""
+
+
