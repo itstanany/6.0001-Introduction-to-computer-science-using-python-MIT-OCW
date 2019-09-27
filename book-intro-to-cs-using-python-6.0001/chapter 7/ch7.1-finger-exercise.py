0d2b73ef997f75c73ac5e40242410bbe3fa7c5a8
Ahmad ALy Mohamad ALy Ramadan Tanany <AhmadAlyMohamad@gmail.com>
2019-09-27 20:17:03 +0200

    adding the exercise solution

--- book-intro-to-cs-using-python-6.0001/chapter 7/ch7.1-finger-exercise.py
+++ book-intro-to-cs-using-python-6.0001/chapter 7/ch7.1-finger-exercise.py
@@ -24,3 +24,29 @@ For example, if s is 'a2b3c' it returns 5"""
 
 '''
 
+# the solution
+
+# string containing all decimal digits
+digits = '0123456789'
+def sumDigits(s):
+    '''
+    Assumes s is a string
+    Returns the sum of the decimal digits in s
+    For example, if s is 'a2b3c' it returns 5
+    '''
+    # variable to save the sum of decimal digits
+    sum = 0
+    #loop through each index of the string argument s
+    for e in s:
+        # try to adding the ndex value to the sum
+        try:
+            # casting "e" variable from string to integer number
+            e = int(e)
+            # if it's integer and raises no exceptions, adding the value to the previous value
+            sum = sum + e
+        # if the "e" value isn't decimal, return the sum with no operations
+        except ValueError:
+            sum = sum
+    # finally, return the sum value 
+    return sum
+
