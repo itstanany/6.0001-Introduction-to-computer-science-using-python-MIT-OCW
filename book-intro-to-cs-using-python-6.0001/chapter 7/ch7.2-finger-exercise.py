d09c3273a8d8c82561024b9536cfa0e108fe8222
Ahmad ALy Mohamad ALy Ramadan Tanany <AhmadAlyMohamad@gmail.com>
2019-09-27 22:16:12 +0200

    adding the exercise to the file

--- book-intro-to-cs-using-python-6.0001/chapter 7/ch7.2-finger-exercise.py
+++ book-intro-to-cs-using-python-6.0001/chapter 7/ch7.2-finger-exercise.py
@@ -1,7 +1,25 @@
 # -*- coding: utf-8 -*-
-"""
+'''
 Created on Fri Sep 27 22:13:22 2019
 
 @author: Ahmed Ali Mohamed
-"""
+
+*******
+
+Finger Exercise 
+Chapter 7 section 2
+
+*******
+
+*******
+
+Finger Exercise: Implement a function that satisfies the specification
+def findAnEven(L):
+"""Assumes L is a list of integers
+Returns the first even number in L
+Raises ValueError if L does not contain an even number"""
+
+
+*******
+'''
 
