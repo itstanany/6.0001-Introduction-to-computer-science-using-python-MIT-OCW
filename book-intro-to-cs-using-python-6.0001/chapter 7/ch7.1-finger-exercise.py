a1875ed7e9f8af66a23b326f7f74ffe1676fdbc0
Ahmad ALy Mohamad ALy Ramadan Tanany <AhmadAlyMohamad@gmail.com>
2019-09-27 20:15:35 +0200

    adding the exercise to the file

--- book-intro-to-cs-using-python-6.0001/chapter 7/ch7.1-finger-exercise.py
+++ book-intro-to-cs-using-python-6.0001/chapter 7/ch7.1-finger-exercise.py
@@ -1,7 +1,26 @@
 # -*- coding: utf-8 -*-
-"""
+'''
 Created on Fri Sep 27 20:05:54 2019
 
 @author: Ahmed Ali Mohamed
-"""
+*******
+
+Finger exercise chapter 7 section 1
+
+*******
+
+
+
+*******
+
+Finger exercise: Implement a function that meets the specification below. Use a
+try-except block.
+def sumDigits(s):
+"""Assumes s is a string
+Returns the sum of the decimal digits in s
+For example, if s is 'a2b3c' it returns 5"""
+
+*******
+
+'''
 
