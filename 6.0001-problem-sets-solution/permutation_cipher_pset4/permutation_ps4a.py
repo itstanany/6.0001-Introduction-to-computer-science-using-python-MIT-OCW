f17c2576c539760df048b49b75138d99e6b053d0
Ahmed-Ali-Mohamed <41123285+Ahmed-Ali-Mohamed@users.noreply.github.com>
2020-06-06 16:13:33 +0200

    implementing get_permutations functn

--- 6.0001-problem-sets-solution/permutation_cipher_pset4/permutation_ps4a.py
+++ 6.0001-problem-sets-solution/permutation_cipher_pset4/permutation_ps4a.py
@@ -22,7 +22,26 @@ def get_permutations(sequence):
     a different order than what is listed here.
     '''
 
-    pass #delete this line and replace with your code here
+    # N is len(sequence)
+    #the base case, no more recursion
+    # return one charchter as a sinlge string element of a list
+    if len(sequence) == 1:
+        return [sequence]
+    #get the permutations of the sequence N-1
+    perms = get_permutations(sequence[1:])
+    #get the first charchter to add itin every possible index of N-1 permutations to get N permutation
+    first_char = sequence[0]
+    #store all result of permutations to be the return value of the function
+    result = [] 
+    #to get the permutation of sequence N, add the first left charchter to the permutations of sequence N-1 
+    for perm in perms:
+        #add the first charchter to one of the sequence N-1 permutations
+        #we construct the range containing len(perm)+! because
+        # becaue we want to insert the first chrachter in the beginning of a permutation and at the end and each index in betwee
+        for i in range(len(perm)+1):
+            result.append(perm[:i]+first_char+perm[i:])
+    #the return value is list of all permutatoins
+    return result
 
 if __name__ == '__main__':
 #    #EXAMPLE
