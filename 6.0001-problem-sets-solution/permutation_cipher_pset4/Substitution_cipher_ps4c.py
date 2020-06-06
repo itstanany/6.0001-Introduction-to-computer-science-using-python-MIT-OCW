63ecc8122fd6c90d4fb5ea79608d816b13f820d7
Ahmed-Ali-Mohamed <41123285+Ahmed-Ali-Mohamed@users.noreply.github.com>
2020-06-06 16:45:43 +0200

    implementing init functn of EncryptedSubMessage class

--- 6.0001-problem-sets-solution/permutation_cipher_pset4/Substitution_cipher_ps4c.py
+++ 6.0001-problem-sets-solution/permutation_cipher_pset4/Substitution_cipher_ps4c.py
@@ -166,7 +166,7 @@ class EncryptedSubMessage(SubMessage):
             self.message_text (string, determined by input text)
             self.valid_words (list, determined using helper function load_words)
         '''
-        pass #delete this line and replace with your code here
+        SubMessage.__init__(self, text)
 
     def decrypt_message(self):
         '''
